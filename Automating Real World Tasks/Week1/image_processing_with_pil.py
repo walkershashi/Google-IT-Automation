b3788ce622d0274c3e29fcf633d15f03600d623b
shashiwalker <skssunny30@gmail.com>
2020-06-21 13:12:26 +0530

    Extracted images and saved the processed images in the Processed_images directory

--- Automating Real World Tasks/Week1/image_processing_with_pil.py
+++ Automating Real World Tasks/Week1/image_processing_with_pil.py
@@ -4,8 +4,10 @@ from PIL import Image
 size = 128, 128
 for f in glob.glob(os.getcwd()+"/images/ic_*"):
   im = Image.open(f).convert('RGB')
-  print(f)
-  print(im.format)
-  new_name = os.path.basename(f)
+  print(f) # optional
+  print(im.format) # optional
+  new_name = os.path.basename(f) # Rename
+  # Extract the images from zip file into "images" directory
+  # Make sure you have created an empty directory "Processed_image" to store the  result images
   im.rotate(270).resize((size)).save(os.getcwd() + "/Processed_image/" + new_name, "JPEG")
 
